e0079e4040d8a612c7c5dd5383d0d84501cefd8d
raulgs24 <raulgs@correo.ugr.es>
2026-04-29 17:15:24 +0200

    Añado css index

--- pfinal/resources/views/index.blade.php
+++ pfinal/resources/views/index.blade.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Origenia</title>
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
 
 </head>
 <body>
     <header>
-        <h1>Origenia</h1>    
+        <h1>Origenia</h1>
+            
 
     </header>   
     <main>
@@ -17,9 +19,9 @@
             <p>Artesanía local, sostenible y con alma. Descubre piezas únicas.</p>
         </section>
         <section>
-            <div>Materiales Sostenibles</div>
-            <div>Hechos a mano en nuestra región</div>
-            <div>Envios directos desde el taller</div>
+            <div class="expl">Materiales Sostenibles</div>
+            <div class="expl">Hechos a mano en nuestra región</div>
+            <div class="expl">Envios directos desde el taller</div>
         </section>
     </main>
 </body>
