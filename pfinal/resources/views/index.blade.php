96b5a0fc1c35c69f6dcfc4e8259ed52d25fd482d
raulgs24 <raulgs@correo.ugr.es>
2026-04-29 16:55:43 +0200

    Primeros pasos

---
+++ pfinal/resources/views/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Origenia</title>
+
+</head>
+<body>
+    <header>
+        <h1>Origenia</h1>    
+
+    </header>   
+    <main>
+        <section>
+            <h2>"Donde las manos cuentan historias."</h2>
+            <p>Artesanía local, sostenible y con alma. Descubre piezas únicas.</p>
+        </section>
+        <section>
+            <div>Materiales Sostenibles</div>
+            <div>Hechos a mano en nuestra región</div>
+            <div>Envios directos desde el taller</div>
+        </section>
+    </main>
+</body>
+</html>
